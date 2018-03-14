5e2d5fcf5219bf67e3a74b8e12d91c4c82d8f685
Will <apricotsenator@gmail.com>
2018-03-14 19:52:28 -0400

    basic-folder-linked-files-firebase

---
+++ app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAA340TsjEt9xMesMiovSSKh2GaWaynRHU",
+    authDomain: "enlighten-up-dc253.firebaseapp.com",
+    databaseURL: "https://enlighten-up-dc253.firebaseio.com",
+    projectId: "enlighten-up-dc253",
+    storageBucket: "",
+    messagingSenderId: "979543628414"
+  };
+  firebase.initializeApp(config);
+
+  console.log("hello")
