8aad4fa8475a74d18a654bc8acc583dc9defa44f
Will <apricotsenator@gmail.com>
2018-03-14 21:15:12 -0400

    working-api-queries-for-memes-news

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ var config = {
   firebase.initializeApp(config);
 
   console.log("hello")
+
+  var url = "https://newsapi.org/v2/top-headlines?" +
+         "sources=bbc-news&" +
+         "apiKey=85680e66dfa24f7bbb4bbc364c98c63a";
+var req = new Request(url);
+fetch(req)
+   .then(function(response) {
+       console.log(response.json());
+   })
+
+  var input = "dogs";
+  var meme = "http://version1.api.memegenerator.net//Generators_Search?q=" + input + "&pageIndex=0&pageSize=12&apiKey=b939f19b-e825-43d7-a423-a52dd5a7b20e"
+  
+  var req = new Request(meme);
+fetch(req)
+   .then(function(response) {
+       console.log(response.json());
+   })
+
