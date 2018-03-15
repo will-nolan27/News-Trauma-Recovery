be1b6de9c337a64bba917dcec8c1a686ab2777f3
Kerrick Woyshner <kerrickwoyshner@CWRU-Sierra-10ddb1bb4306.local>
2018-03-14 21:38:07 -0400

    Included Dad Joke API URL & AJAX Request

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var dadQueryURL = "https://icanhazdadjoke.com/search";
+var input;
+// Dad joke api ajax request
+$.ajax({
+  url: dadQueryURL,
+  method: "get", //send it through get method
+  data: { 
+    page: 1, 
+    limit: 20,
+    term: input
+  },
+});
+
 var config = {
     apiKey: "AIzaSyAA340TsjEt9xMesMiovSSKh2GaWaynRHU",
     authDomain: "enlighten-up-dc253.firebaseapp.com",
